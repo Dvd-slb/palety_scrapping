52c75e7eafb2b853a13eaea61a9f9b2fed878d6a
David Slaboň <slabon.david94@gmail.com>
2023-02-12 19:50:26 +0100

    Dolů do poznámek v Pipeline přidán postup na stažení souboru s detaily jednotlivých boxů

--- paleto_scrapy/pipelines.py
+++ paleto_scrapy/pipelines.py
@@ -2,8 +2,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
+import requests
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import sqlite3
@@ -21,11 +20,6 @@ class PaletoScrapyPipeline:
         self.cursor = self.connection.cursor()
 
     def create_table(self):
-        # id
-        # INTEGER
-        # PRIMARY
-        # KEY,
-
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS items(
                         id INTEGER PRIMARY KEY,
                         name TEXT,
@@ -36,50 +30,48 @@ class PaletoScrapyPipeline:
                         category TEXT,
                         most_expensive_item_q INTEGER,
                         most_expensive_item_price INTEGER,
+                        most_expensive_item_desc TEXT,
                         most_items_q INTEGER,
                         most_items_price INTEGER,
+                        most_items_desc TEXT,
                         total_q INTEGER,
                         total_price INTEGER
                         )""")
-        # self.connection.commit()
-        # self.connection.close()
-        # self.cursor.execute("INSERT INTO items VALUES (name=?)", ('S1581', ))
-        # self.connection.commit()
 
     def process_item(self, item, spider):
         self.store_db(item)
         return item
 
     def store_db(self, item):
-
         self.cursor.execute("SELECT * FROM items WHERE name=?", (item["name"], ))
         if self.cursor.fetchone() is None:
             self.cursor.execute("""INSERT INTO items VALUES (:id, :name, :link, :down_link, :dead_line, :max_bid, :category,
-                :most_expensive_item_q, :most_expensive_item_price, :most_items_q, :most_items_price,
-                :total_q, :total_price)""", {"id": None, "name": item["name"], "link": item["link"],
-                "down_link": item["down_link"], "dead_line": item["dead_line"], "max_bid": item["max_bid"],
-                "category": item["category"], "most_expensive_item_q": False, "most_expensive_item_price": False,
-                "most_items_q": False, "most_items_price": False, "total_q": False, "total_price": False})
-        else:
-            print("Tohle tam už jeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+                :most_expensive_item_q, :most_expensive_item_price, :most_expensive_item_desc, :most_items_q,
+                :most_items_price, :most_items_desc, :total_q, :total_price)""", {"id": None, "name": item["name"],
+                    "link": item["link"], "down_link": item["down_link"], "dead_line": item["dead_line"],
+                    "max_bid": item["max_bid"], "category": item["category"], "most_expensive_item_q": False,
+                    "most_expensive_item_price": False, "most_expensive_item_desc": False, "most_items_q": False,
+                    "most_items_price": False, "most_items_desc": False, "total_q": False, "total_price": False})
 
-        self.cursor.execute("SELECT * FROM items")
-        print(self.cursor.fetchall())
-        # self.cursor.execute("SELECT * FROM items WHERE name=?", (item["name"], ))
-        # if self.cursor.fetchone() is None:
-        #     self.cursor.execute("""INSERT into items VALUES (name=?)""", (item["name"], ))
-        # else:
-        #     print("not ok")
 
-        # self.cursor.execute("SELECT * FROM items WHERE name=?", (item["name"],))
-        # print(self.cursor.fetchall())
-        # self.connection.commit()
-        # self.connection.close()
         self.connection.commit()
+
+    # def download_file(self):
+    #     self.cursor.execute("SELECT down_link from items")
+
+
+# _______________ POSTUP NA STAŽENÍ XLSX SOUBORU S DETAILY JEDNOTLIVÝCH PALET ________________
+# cursor.execute("SELECT down_link, name FROM items")
+# for f in cursor.fetchall():
+#     url = f[0]
+#     name = f[1]
+#     file = requests.get(url)
+#     with open(f"box_description/{name}.xlsx", "wb") as box_details:
+#         box_details.write(file.content)
 
 
 # _________________________________  PŘEVOD   NA   DATATIME  _________________________________
-# date_string = '14. 2. 2023 18:44'
-# date_format = "%d. %m. %Y %H:%M"
+# date_string = '14. 2. 2023 18:44:00'
+# date_format = "%d. %m. %Y %H:%M:%S"
 # date_parsed = datetime.strptime(date_string, date_format)
 # print(date_parsed)
--- test.py
+++ test.py
@@ -1,18 +1,48 @@
 import sqlite3
+import requests
 from dateutil.parser import parse
 from datetime import datetime
 
-# connection = sqlite3.connect("../data.db")
-connection = sqlite3.connect(":memory:")
+connection = sqlite3.connect("../data.db")
+# connection = sqlite3.connect(":memory:")
 cursor = connection.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS items(
-                        id INTEGER PRIMARY KEY,
-                        name TEXT,
-                        link TEXT,
-                        down_link TEXT,
-                        dead_line DATETIME
-                        )""")
+# cursor.execute("SELECT * FROM items")
+# print(cursor.fetchall())
+# cursor.execute("SELECT * FROM items")
+# print(len(cursor.fetchall()))
+
+
+# _________ STAŽENÍ SOUBORU Z LINKU Z DATABÁZE ________
+# cursor.execute("SELECT down_link, name FROM items")
+# for f in cursor.fetchall():
+#     url = f[0]
+#     name = f[1]
+#     file = requests.get(url)
+#     with open(f"box_description/{name}.xlsx", "wb") as box_details:
+#         box_details.write(file.content)
+
+cursor.execute("SELECT * FROM items WHERE name=?", ("S1955", ))
+print(cursor.fetchall())
+
+# cursor.execute("SELECT down_link FROM items")
+# down_link = cursor.fetchall()
+# cursor.execute("SELECT name FROM items")
+# name = cursor.fetchall()
+# print(down_link)
+# print(name)
+# file = requests.get(cursor.fetchall()
+
+connection.commit()
+connection.close()
+
+# cursor.execute("""CREATE TABLE IF NOT EXISTS items(
+#                         id INTEGER PRIMARY KEY,
+#                         name TEXT,
+#                         link TEXT,
+#                         down_link TEXT,
+#                         dead_line DATETIME
+#                         )""")
                         # max_bid INTEGER,
                         # category TEXT,
                         # most_expensive_item_q INTEGER,
@@ -23,23 +53,25 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS items(
                         # total_price INTEGER
                         # )""")
 
-cursor.execute("PRAGMA table_info(items)")
-column_names = [row[1] for row in cursor.fetchall()]
-print(column_names)
+# cursor.execute("PRAGMA table_info(items)")
+# column_names = [row[1] for row in cursor.fetchall()]
+# print(column_names)
 
 # cursor.execute("ALTER TABLE items ADD name TEXT")
 
-cursor.execute("INSERT INTO items VALUES (:id, :name, :link, :down_link, :dead_line)", {"id": None, "name": "kokot", "link": "neznam",
-                                                                                   "down_link": False, "dead_line": False})
+# cursor.execute("INSERT INTO items VALUES (:id, :name, :link, :down_link, :dead_line)", {"id": None, "name": "kokot", "link": "neznam",
+#                                                                                    "down_link": False, "dead_line": False})
 
 # cursor.execute("INSERT INTO items VALUES (:id, :name, :link, :down_link, :dead_line)", {"id": None, "name": "yep", "link": "vim",
 #                                                                                    "down_link": False, "dead_line": "nikdy"})
 
-cursor.execute("SELECT * FROM items")
-print(cursor.fetchall())
-
-connection.commit()
-connection.close()
+# cursor.execute("SELECT * FROM items")
+# print(cursor.fetchall())
+# cursor.execute("SELECT * FROM items")
+# print(len(cursor.fetchall()))
+#
+# connection.commit()
+# connection.close()
 
 # date_string = '14. 2. 2023 18:44:00'
 # date_format = "%d. %m. %Y %H:%M:%S"
